48ea2d04e4c3e96794348624cd55499591e042ca
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2024-10-22 14:26:49 +0300

    add QRCode decoder

---
+++ VCardGenerator/Controllers/QRScannerController.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using VCardGenerator.ViewModels;
+using ZXing;
+using ZXing.Windows.Compatibility;
+
+namespace VCardGenerator.Controllers
+{
+    public class QRScannerController : Controller
+    {
+        // GET: QRScannerController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(QRScannerViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using var stream = new MemoryStream();
+                await model.QRCode.CopyToAsync(stream);
+
+                var bitmap = new Bitmap(stream);
+                BitmapLuminanceSource source = new BitmapLuminanceSource(bitmap);
+                var reader = new BarcodeReaderGeneric();
+                var decodedValue = reader.Decode(source);
+                model.Text = decodedValue.Text;
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ VCardGenerator/ViewModels/QRScannerViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VCardGenerator.ViewModels
+{
+    public class QRScannerViewModel
+    {
+        [Required]
+        public IFormFile QRCode { get; set; } = null!;
+        public string Text { get; set; } = string.Empty;
+    }
+}
